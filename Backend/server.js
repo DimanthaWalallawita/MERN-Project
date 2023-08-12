ac57cc8c45931816faaeb697491788c798ee091d
DimanthaWalallawita <walallawitad@gmail.com>
2023-08-12 15:47:38 -0700

    init commit

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(
     cors({
-      origin: ["http://localhost:5000"],
+      origin: ["http://localhost:3000"],
       credentials: true,
     })
   );
